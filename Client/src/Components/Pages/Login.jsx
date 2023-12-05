cd25769699f951092cac4022cffa9cceb4744821
simran singh <simran27@bu.edu>
2023-12-05 18:42:51 -0500

    Removed GoogleCredentials import in Login.js

--- Client/src/Components/Pages/Login.jsx
+++ Client/src/Components/Pages/Login.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+import { GoogleLogin } from '@react-oauth/google'
+import { useGoogleOAuth } from '@react-oauth/google'
 
 export const Login = () => {
   return (
-    <div>
+    <div className='Login'>
       <br></br>
       <br></br>
       Login
